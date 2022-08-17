7dd5b56c471334dbdcbf8f74c8d7638e8bc68127
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-08-17 12:16:57 +0400

    feat(root): add cassandra_user_config

--- ROADMAP.md
+++ ROADMAP.md
@@ -6,7 +6,7 @@ This document describes the current status of this project.
 | Status | Goal | Labels | 
 | :---: | :--- | --- | 
 | ✔ | [Set up basic module features]() || 
-| ❌ | [Add cassandra_user_config]() ||
+| ✔ | [Add cassandra_user_config]() ||
 | ❌ | [Add cassandra_user_config.cassandra]() ||
 | ❌ | [Add cassandra_user_config.private_access]() ||
 | ❌ | [Add cassandra_user_config.public_access]() ||
--- main.tf
+++ main.tf
@@ -10,6 +10,14 @@ resource "aiven_cassandra" "this" {
   service_name            = var.service_name
   termination_protection  = var.termination_protection
 
+  cassandra_user_config {
+    cassandra_version     = var.cassandra_version
+    ip_filter             = var.ip_filter
+    migrate_sstableloader = var.migrate_sstableloader
+    project_to_fork_from  = var.project_to_fork_from
+    service_to_fork_from  = var.service_to_fork_from
+  }
+
 
   dynamic "tag" {
     for_each = var.tags
